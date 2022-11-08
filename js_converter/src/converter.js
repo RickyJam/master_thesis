8452d8532d5605e1a6506db65bf3d6efba9159d6
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-11-08 16:05:46 +0100

    refactor

--- js_converter/src/converter.js
+++ js_converter/src/converter.js
@@ -1,6 +1,4 @@
-const converter = require("convert-csv-to-json");
-const Path = require("path");
-const FS = require("fs");
+const { convertAllFiles } = require("./fileSystemHandler");
 const { isInvalidDir } = require("./utils");
 
 // ignoring 0 and 1, they are std arguments
@@ -9,27 +7,11 @@ const DESTINATION_PATH_ARG = 3;
 
 const startDir = process.argv[START_PATH_ARG];
 const destinationDir = process.argv[DESTINATION_PATH_ARG];
-if(isInvalidDir(startDir) || isInvalidDir(destinationDir)) {
-    console.log("Please provide a directory path as follow:");
-    console.log("node converter.js path/to/dataset_dir path/to/destination_dir");
-    console.log("Exiting...");
-    return;
+if (isInvalidDir(startDir) || isInvalidDir(destinationDir)) {
+  console.log("Please provide a directory path as follow:");
+  console.log("node converter.js path/to/dataset_dir path/to/destination_dir");
+  console.log("Exiting...");
+  return;
 }
 
-function throughDirectory(startDir) {
-//   FS.readdirSync(startDir).forEach((file) => {
-//     const Absolute = Path.join(startDir, file);
-//     if (FS.statSync(Absolute).isDirectory()) {
-//       return ThroughDirectory(Absolute);
-//     } else {
-//       return files.push(Absolute);
-//     }
-//   });
-}
-
-throughDirectory(startDir);
-
-// let fileInputName = "myInputFile.csv";
-// let fileOutputName = "myOutputFile.json";
-
-// converter.generateJsonFileFromCsv(fileInputName, fileOutputName);
+convertAllFiles(startDir, destinationDir);
