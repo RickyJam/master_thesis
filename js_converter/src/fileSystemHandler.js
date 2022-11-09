8460a135fe5390e9f0eda2e64e3ec7fd87f782d5
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-11-09 21:16:45 +0100

    completato parser json

--- js_converter/src/fileSystemHandler.js
+++ js_converter/src/fileSystemHandler.js
@@ -1,6 +1,11 @@
-const { generateJsonFileFromCsv } = require("convert-csv-to-json");
+const converter = require("convert-csv-to-json");
 const Path = require("path");
 const FS = require("fs");
+
+const stringToBeReplaced =
+  "Date&Time,use[kW],gen[kW],FurnaceHRV[kW],CellarOutlets[kW],WashingMachine[kW],FridgeRange[kW],DisposalDishwasher[kW],KitchenLights[kW],BedroomOutlets[kW],BedroomLights[kW],MasterOutlets[kW],MasterLights[kW],DuctHeaterHRV[kW]";
+const replacementString =
+  "dateTime,usedKw,generateKw,furnaceHRVKw,cellarOutletsKw,washingMachineKw,fridgeRangeKw,disposalDishwasherKw,kitchenLightsKw,bedroomOutletsKw,bedroomLightsKw,masterOutletsKw,masterLightsKw,ductHeaterHRVKw";
 
 function throughDirectory(currentDir, destinationDir) {
   FS.readdirSync(currentDir).forEach((file) => {
@@ -10,27 +15,34 @@ function throughDirectory(currentDir, destinationDir) {
       createDir(absoluteDestinationPath);
       throughDirectory(absolutePath, absoluteDestinationPath);
     } else {
-      convertFile(absolutePath);
+      convertFile(absolutePath, file, destinationDir);
     }
   });
 }
 
-function convertFile(fileInputName) {
+function replaceHeaderLine(file) {
+  const data = FS.readFileSync(file, { encoding: "utf8", flag: "r" });
+  const result = data.replace(stringToBeReplaced, replacementString);
+  FS.writeFileSync(file, result, { encoding: "utf8" });
+}
+
+function convertFile(fileInputName, file, destinationDir) {
   if (file.startsWith(".")) {
     return;
   }
+
   const jsonFileName = file.split(".")[0] + ".json";
   const fileOutputName = Path.join(destinationDir, jsonFileName);
 
-  //TODO: riscrivere prima riga
+  replaceHeaderLine(fileInputName);
 
-  generateJsonFileFromCsv(fileInputName, fileOutputName);
+  converter.fieldDelimiter(",");
+  converter.generateJsonFileFromCsv(fileInputName, fileOutputName);
   return;
-
 }
 
 function createDir(dirPath) {
-  if(!FS.existsSync(dirPath)) {
+  if (!FS.existsSync(dirPath)) {
     FS.mkdirSync(dirPath);
   }
 }
