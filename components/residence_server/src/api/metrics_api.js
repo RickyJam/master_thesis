f18f8d9138c3deb9d648deddb2d8ee84856fb19a
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-11-25 13:53:32 +0100

    create prime api

--- components/residence_server/src/api/metrics_api.js
+++ components/residence_server/src/api/metrics_api.js
@@ -1,2 +1,23 @@
-const MetricsApi = null;
+METRICS_PATH = "/metrics";
+
+const MetricsApi = (server) => ({
+    register: () => {
+        server.get('/residence', (req, res) => {
+            //TODO: chiamata al service
+        });
+
+        server.get('/residence/kitchens', (req, res) => {
+            //TODO: chiamata al service
+        });
+
+        server.get('/residence/laundry', (req, res) => {
+            //TODO: chiamata al service
+        });
+
+        server.get('/residence/power', (req, res) => {
+            //TODO: chiamata al service
+        });
+    }
+});
+
 export default MetricsApi;
--- components/residence_server/src/server.js
+++ components/residence_server/src/server.js
@@ -1,2 +1,7 @@
-//TODO: entry point del server
+import MetricsApi from "./api/metrics_api";
+import express from "express";
 
+const server = express();
+const metrics_api = MetricsApi(server);
+
+metrics_api.register();
