b0bbb941a32d9b92e5959efd1357fa79e17153cc
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-11-27 21:19:13 +0100

    creato servizio vuoto

--- components/residence_server/server.js
+++ components/residence_server/server.js
@@ -1,5 +1,6 @@
 import MetricsApi from "./src/api/metrics_api.js";
 import express from "express";
+import isDev from "./src/utils/enviroment.js";
 
 const PORT = 3000;
 
@@ -8,4 +9,11 @@ const metrics_api = MetricsApi(server);
 
 metrics_api.register();
 
-server.listen(PORT, () => console.log('Node server started and listening on port 3000'));
+server.listen(PORT, () =>
+  console.log("Node server started and listening on port 3000")
+);
+
+server.get("/", async (req, res) => {
+  const enviroment = isDev() ? "DEV" : "PROD";
+  res.send(`OK - ${enviroment}`);
+});
