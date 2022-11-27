ddc6070a00f70a20ac0fb94104d27c080086f8b2
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-11-27 19:24:15 +0100

    connessione in base a prod o dev

--- components/residence_server/src/dao/mongo_access.js
+++ components/residence_server/src/dao/mongo_access.js
@@ -1,21 +1,25 @@
 import { MongoClient } from "mongodb";
+import isDev from "../utils/enviroment.js";
 
-// const uri = "mongodb+srv://sample-hostname:27017/?maxPoolSize=20&w=majority";
 const DB_NAME = "master";
 const COLLECTION_NAME = "measurements";
+const k8s_host = 'mongodb-service';
+const dev_host = 'localhost:27017';
 
-const uri =
-  "mongodb://admin:password@localhost:27017/?authMechanism=DEFAULT&authSource=" +
-  DB_NAME;
+function getDbUri() {
+  const hostname = isDev() ? dev_host : k8s_host;
+  return `mongodb://admin:password@${hostname}/?authMechanism=DEFAULT&authSource=${DB_NAME}`;
+}
 
 async function getLastTenMetrics() {
+  const uri = getDbUri();
   const client = new MongoClient(uri);
   try {
     await client.connect();
     const db = client.db(DB_NAME);
     const collection = db.collection(COLLECTION_NAME);
 
-    return  await collection.find().limit(10).toArray();
+    return await collection.find().limit(10).toArray();
   } catch {
     console.log("ERRORE RISCONTRATO");
     return [];
