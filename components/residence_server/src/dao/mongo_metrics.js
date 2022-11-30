61e7d2a701035d39acdcfe72122ea4b7e54b93ce
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-11-30 22:04:03 +0100

    refactor

--- components/residence_server/src/dao/mongo_metrics.js
+++ components/residence_server/src/dao/mongo_metrics.js
@@ -1,15 +1,25 @@
 import getMongoClient, { DB_NAME } from "./mongo_access.js";
 
 async function getLastTenMetricsFrom(collectionName) {
+  return await onMasterDB((db) =>
+    db.collection(collectionName).find().limit(10).toArray()
+  );
+}
+
+async function getKitchenMetrics(collectionName) {
+  return onMasterDB((db) => {});
+}
+
+async function onMasterDB(callback) {
   const client = getMongoClient();
   try {
     await client.connect();
     const db = client.db(DB_NAME);
 
-    return await db.collection(collectionName).find().limit(10).toArray();
+    return await callback(db);
   } catch {
     console.log("Error");
-    return [];
+    return undefined;
   } finally {
     await client.close();
   }
