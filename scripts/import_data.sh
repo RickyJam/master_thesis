441e319d63c55398c4499ce241e04135151f4b2d
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-01 11:08:10 +0100

    modificato file sh

--- scripts/import_data.sh
+++ scripts/import_data.sh
@@ -1,62 +1,40 @@
 #!/bin/sh
 
-DB_MASTER="master"
-MONGO_MASTER_USERNAME="admin"
-MONGO_MASTER_PASSWORD="password"
+import_mongo_file() {
+    DB_MASTER="master"
+    MONGO_MASTER_USERNAME="admin"
+    MONGO_MASTER_PASSWORD="password"
+    json_file=$1
+    collection=$2
+    mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $collection --file $json_file --jsonArray;
+}
 
-COLLECTION="homeA"
-DATA_FILE_2014="Datasets/json/HomeA/2014/"
-DATA_FILE_2015="Datasets/json/HomeA/2015/"
-DATA_FILE_2016="Datasets/json/HomeA/2016/"
-cd $DATA_FILE_2014
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2015
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2016
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
+iterate_json_file() {
+    year_folder=$1
+    collection=$2
+    ls -1 $year_folder | while read json_file; do import_mongo_file $year_folder/$json_file $collection; done
+}
 
-COLLECTION="homeB"
-DATA_FILE_2014="Datasets/json/HomeB/2014/"
-DATA_FILE_2015="Datasets/json/HomeB/2015/"
-DATA_FILE_2016="Datasets/json/HomeB/2016/"
-cd $DATA_FILE_2014
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2015
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2016
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
+iterate_year_folder() {
+    home_folder=$1
+    collection=$2
+    ls -1 $home_folder | while read year_folder; do iterate_json_file $home_folder/$year_folder $collection; done
+}
 
-COLLECTION="homeC"
-DATA_FILE_2014="Datasets/json/HomeC/2014/"
-DATA_FILE_2015="Datasets/json/HomeC/2015/"
-DATA_FILE_2016="Datasets/json/HomeC/2016/"
-cd $DATA_FILE_2014
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2015
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2016
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
+HOME_A_DATA_FILE="Datasets/json/HomeA"
+iterate_year_folder $HOME_A_DATA_FILE "homeA"
 
-COLLECTION="homeD"
-DATA_FILE_2015="Datasets/json/HomeD/2015/"
-DATA_FILE_2016="Datasets/json/HomeD/2016/"
-cd $DATA_FILE_2015
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2016
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
+HOME_A_DATA_FILE="Datasets/json/HomeB"
+iterate_year_folder $HOME_A_DATA_FILE "homeB"
 
-COLLECTION="homeE"
-DATA_FILE_2016="Datasets/json/HomeE/2016/"
-cd $DATA_FILE_2016
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
+HOME_A_DATA_FILE="Datasets/json/HomeC"
+iterate_year_folder $HOME_A_DATA_FILE "homeC"
 
-COLLECTION="homeF"
-DATA_FILE_2014="Datasets/json/HomeF/2014/"
-DATA_FILE_2015="Datasets/json/HomeF/2015/"
-DATA_FILE_2016="Datasets/json/HomeF/2016/"
-cd $DATA_FILE_2014
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2015
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
-cd $DATA_FILE_2016
-ls -1 *.json | while read jsonfile; do mongoimport -u $MONGO_MASTER_USERNAME -p $MONGO_MASTER_PASSWORD --db $DB_MASTER --collection $COLLECTION --file $jsonfile --jsonArray; done
+HOME_A_DATA_FILE="Datasets/json/HomeD"
+iterate_year_folder $HOME_A_DATA_FILE "homeD"
+
+HOME_A_DATA_FILE="Datasets/json/HomeE"
+iterate_year_folder $HOME_A_DATA_FILE "homeE"
+
+HOME_A_DATA_FILE="Datasets/json/HomeF"
+iterate_year_folder $HOME_A_DATA_FILE "homeF"
