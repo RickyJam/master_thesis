0cd4dee66813f3b1e3ba4269f7d05043712ee5e2
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-02 17:32:54 +0100

    altro lavoro svolto

--- node_utils/dataset_loader/loader.js
+++ node_utils/dataset_loader/loader.js
@@ -1,5 +1,6 @@
-import FileReader from './src/FileReader.js'
-import LineRecord from './src/LineRecord.js'
+import FileReader from "./src/FileReader.js";
+import LineRecord from "./src/LineRecord.js";
+import FS from "fs";
 
 async function getLineRecordFromFile(fileReader) {
   const line = await fileReader.readLine();
@@ -15,32 +16,55 @@ async function getLineRecordFromFile(fileReader) {
 
 async function getNewLineWithFixedTime(fileReader) {
   let record;
-  
+
   do {
     record = await getLineRecordFromFile(fileReader);
     if (record) {
-      if (record.dateTime.getMinutes() % 30 == 0) {
+      if (record.dateTime.getMinutes() % 30 === 0) {
         return record;
       }
     }
   } while (record != undefined);
-  
+
   return undefined;
 }
 
-async function openFile() {
-  const fileReader = new FileReader();
+async function openFile(filePath) {
+  const fileReader = new FileReader(filePath);
   await fileReader.readHeader();
   return fileReader;
 }
 
-async function job() {
-  const fileReader = await openFile();
-  let record;
-  do {
-    record = await getNewLineWithFixedTime(fileReader);
-    console.log(record.dateTime);
-  } while (record != undefined);
+function emptyLineRecords(arr) {
+  while (arr.length) {
+    arr.pop();
+  }
 }
 
-job();
+async function handleSingleFolder(fileReaders) {
+
+  let haveRecords = true;
+  do {
+    let lineRecords = [];
+    for (const index in fileReaders) {
+      lineRecords.push(await getNewLineWithFixedTime(fileReaders[index]));
+    }
+    haveRecords = lineRecords.every((record) => record != undefined)
+  } while (haveRecords);
+}
+
+async function job(basePath) {
+  const fileReaders = [];
+  const files = FS.readdirSync(basePath);
+  for (const index in files) {
+    if (files[index] === ".DS_Store") {
+      continue;
+    }
+    fileReaders.push(await openFile(basePath + files[index]));
+  }
+
+  await handleSingleFolder(fileReaders);
+}
+
+job("../../Datasets/csv/HomeA/2014/");
+
