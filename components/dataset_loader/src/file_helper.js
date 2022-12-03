2d635d753d39057efc28a41afb6a2a8d39bbf49f
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-03 12:30:01 +0100

    estratte fn in file a parte

---
+++ components/dataset_loader/src/file_helper.js
@@ -0,0 +1,130 @@
+import FileReader from "./file_reader.js";
+import LineRecord from "./line_record.js";
+import FS from "fs";
+import insertDocIn from "./mongo_loader.js";
+
+async function getLineRecordFromFile(fileReader) {
+  const line = await fileReader.readLine();
+
+  if (line) {
+    const record = new LineRecord(line);
+    record.parseLine();
+    return record;
+  }
+
+  return undefined;
+}
+
+async function getNewLineWithFixedTime(fileReader) {
+  let record;
+
+  do {
+    record = await getLineRecordFromFile(fileReader);
+    if (record) {
+      if (record.dateTime.getMinutes() % 30 === 0) {
+        return record;
+      }
+    }
+  } while (record != undefined);
+
+  return undefined;
+}
+
+async function openFile(filePath) {
+  const fileReader = new FileReader(filePath);
+  await fileReader.readHeader();
+  return fileReader;
+}
+
+function mergeLines(lineRecords) {
+  const firstDefinedElement = lineRecords.find(
+    (record) => record !== undefined
+  );
+  if (firstDefinedElement === undefined) return;
+  const mergedLineRecord = firstDefinedElement.deepClone({ header: true });
+  lineRecords.forEach((record) => {
+    if (record) {
+      mergedLineRecord.specificMetrics = [
+        ...mergedLineRecord.specificMetrics,
+        ...record.specificMetrics,
+      ];
+    }
+  });
+  return mergedLineRecord;
+}
+
+function mergeHeaders(fileReaders) {
+  const [first, ...remaining] = fileReaders;
+  let header = first.header.split(",");
+
+  remaining?.forEach((reader) => {
+    const [_, __, ___, ...metricsHeader] = reader.header.split(",");
+    header = [...header, ...metricsHeader];
+  });
+
+  return header;
+}
+
+function insertLineInMongo(mergedHeaders, mergedLine, mongoCollection) {
+  console.log("inserimento di un obj in: " + mongoCollection);
+  const doc = mergedLine.toDocument(mergedHeaders);
+}
+
+async function importFiles(fileReaders, mongoCollection) {
+  const mergedHeaders = mergeHeaders(fileReaders);
+
+  let lineRecords;
+  do {
+    lineRecords = [];
+    for (const index in fileReaders) {
+      lineRecords.push(await getNewLineWithFixedTime(fileReaders[index]));
+    }
+
+    const mergedLine = mergeLines(lineRecords);
+    insertLineInMongo(mergedHeaders, mergedLine, mongoCollection);
+  } while (lineRecords.every((record) => record != undefined));
+}
+
+async function importFolder(path, mongoCollection) {
+  const fileReaders = [];
+  const files = FS.readdirSync(path);
+  for (const index in files) {
+    if (files[index] === ".DS_Store") {
+      continue;
+    }
+    fileReaders.push(await openFile(path + files[index]));
+  }
+
+  await importFiles(fileReaders, mongoCollection);
+}
+
+function importYearFolders(path, mongoCollection) {
+  FS.readdirSync(path).forEach((yearFolderName) => {
+    if (yearFolderName !== ".DS_Store") {
+      importFolder(path + yearFolderName + "/", mongoCollection);
+    }
+  });
+}
+
+// "../../Datasets/csv/"
+function importHomeFolders(basePath) {
+  FS.readdirSync(basePath).forEach((homefolderName) => {
+    if (homefolderName !== ".DS_Store") {
+      importYearFolders(
+        basePath + homefolderName + "/",
+        collectionName[homefolderName]
+      );
+    }
+  });
+}
+
+const collectionName = {
+  HomeA: "homeA",
+  HomeB: "homeB",
+  HomeC: "homeC",
+  HomeD: "homeD",
+  HomeE: "homeE",
+  HomeF: "homeF",
+};
+
+export default importHomeFolders;
