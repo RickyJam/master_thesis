bfba32988b8f5ff82de64f7c5f97977d47849ce4
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-03 12:01:34 +0100

    creata classe per inseri i doc sul db

---
+++ components/dataset_loader/src/mongo_loader.js
@@ -0,0 +1,24 @@
+import getMongoClient from "./mongo_access";
+
+let client;
+export function openConnection() {
+    client = getMongoClient();
+}
+
+export function closeConnection() {
+    client.close();
+}
+
+const insertDocIn = async (collection, doc) => {
+  try {
+    await client.connect();
+    const db = client.db(DB_NAME);
+
+    db.collection(collection).insertOne(doc);
+  } catch {
+    console.log("Error insert in: " + collection);
+    return undefined;
+  }
+};
+
+export default insertDocIn;
