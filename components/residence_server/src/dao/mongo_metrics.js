b709942b3b51edf4b56b8529d5aef3be24d71959
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-05 11:02:41 +0100

    corretta query

--- components/residence_server/src/dao/mongo_metrics.js
+++ components/residence_server/src/dao/mongo_metrics.js
@@ -2,6 +2,8 @@ import { filterParams } from "../utils/mongo_helper.js";
 import getMongoClient, { DB_NAME } from "./mongo_access.js";
 
 const lastDate = new Date(2016, 11, 31, 23, 59, 59, 0);
+const asc = 1;
+const desc = -1;
 
 async function getLastTenMetricsFrom(collectionName) {
   return await onMasterDB((db) =>
@@ -9,7 +11,7 @@ async function getLastTenMetricsFrom(collectionName) {
   );
 }
 
-async function getKitchenMetrics(collectionName, date=lastDate) {
+async function getKitchenMetrics(collectionName, sort = asc, date = lastDate) {
   const lastWeekDate = new Date(date);
   lastWeekDate.setDate(date.getDate() - 7);
   return await onMasterDB((db) =>
@@ -17,25 +19,26 @@ async function getKitchenMetrics(collectionName, date=lastDate) {
       .collection(collectionName)
       .find(
         {
-          dateField: {
+          dateTime: {
             $lt: date,
             $gte: lastWeekDate,
           },
         },
         filterParams[collectionName].kitchen
       )
-      .limit(1)
+      .sort({ dateTime: desc })
+      .limit(10)
       .toArray()
   );
 }
 
-async function onMasterDB(callback) {
+async function onMasterDB(query) {
   const client = getMongoClient();
   try {
     await client.connect();
     const db = client.db(DB_NAME);
 
-    return await callback(db);
+    return await query(db);
   } catch {
     console.log("Error");
     return undefined;
