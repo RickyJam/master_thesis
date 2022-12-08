768a265d3df0dabb5d2a9e7c57885210a5a5f3c9
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-08 17:53:14 +0100

    introdotta validazione home

--- components/residence_server/src/api/sensors_api.js
+++ components/residence_server/src/api/sensors_api.js
@@ -1,4 +1,5 @@
 import SensorsService from "../services/sensors_service.js";
+import collections from "../utils/mongo_helper.js";
 
 const sensorsService = SensorsService();
 
@@ -12,23 +13,39 @@ const SensorsApi = (server) => ({
   register: () => {
     server.get(SENSORS_PATH, async (req, res) => {
       const home = req.params[HOME_KEY];
+      if (!validateHomeParam(home)) {
+        res.status(404).send("Unvalid home provided - " + home);
+      }
+
       const data = await sensorsService.getHomeSensors();
       res.send(data);
     });
 
     server.get(KITCHENS_PATH, async (req, res) => {
       const home = req.params[HOME_KEY];
+      if (!validateHomeParam(home)) {
+        res.status(404).send("Unvalid home provided - " + home);
+      }
+
       const data = await sensorsService.getHomeKitchenSensors();
       res.send(data);
     });
 
     server.get(LAUNDRY_PATH, async (req, res) => {
       const home = req.params[HOME_KEY];
+      if (!validateHomeParam(home)) {
+        res.status(404).send("Unvalid home provided - " + home);
+      }
+
       const data = await sensorsService.getHomeLaundrySensors();
       res.send(data);
     });
   },
 });
+
+function validateHomeParam(homeParam) {
+  return collections.hasOwnProperty(homeParam);
+}
 
 export const getSensorsPaths = [SENSORS_PATH, KITCHENS_PATH, LAUNDRY_PATH];
 export default SensorsApi;
