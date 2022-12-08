862b852704d95aae7dafb7a7026f730a23bc45f4
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-08 17:19:47 +0100

    rename all metrics in consumption

--- components/residence_server/server.js
+++ components/residence_server/server.js
@@ -1,13 +1,13 @@
-import MetricsApi, { getConsumptionPaths } from "./src/api/consumption_api.js";
+import ConsumptionApi, { getConsumptionPaths } from "./src/api/consumption_api.js";
 import express from "express";
 import isDev from "./src/utils/enviroment.js";
 
 const PORT = 3000;
 
 const server = express();
-const metrics_api = MetricsApi(server);
+const consumption_api = ConsumptionApi(server);
 
-metrics_api.register();
+consumption_api.register();
 
 server.listen(PORT, () =>
   console.log("Node server started and listening on port 3000")
