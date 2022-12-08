18d9acadacda01c1c22fdd65618be5182c0fbb1e
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-08 17:36:21 +0100

    creato service

---
+++ components/residence_server/src/services/sensors_service.js
@@ -0,0 +1,53 @@
+import collections from "../utils/mongo_helper.js";
+
+const { HOMEA, HOMEB, HOMEC, HOMED, HOMEE, HOMEF } = collections;
+
+const lastDate = new Date(2016, 11, 31, 23, 59, 59, 0);
+const ASC = 1;
+const DESC = -1;
+
+const SensorsService = () => ({
+  getHomeConsumption: async () => {
+    const data = {
+      HOMEA: null, // await getLastTenConsumptionFrom(HOMEA),
+      HOMEB: null, // await getLastTenConsumptionFrom(HOMEB),
+      HOMEC: null, // await getLastTenConsumptionFrom(HOMEC),
+      HOMED: null, // await getLastTenConsumptionFrom(HOMED),
+      HOMEE: null, // await getLastTenConsumptionFrom(HOMEE),
+      HOMEF: null, // await getLastTenConsumptionFrom(HOMEF),
+    };
+    return { data };
+  },
+  getResidanceKitchensConsumption: async (toDate = lastDate, sort = DESC) => {
+    const fromDate = getLastMonthDate(toDate);
+    const data = {
+      HOMEA: null, // await getLastTenConsumptionFrom(HOMEA),
+      HOMEB: null, // await getLastTenConsumptionFrom(HOMEB),
+      HOMEC: null, // await getLastTenConsumptionFrom(HOMEC),
+      HOMED: null, // await getLastTenConsumptionFrom(HOMED),
+      HOMEE: null, // await getLastTenConsumptionFrom(HOMEE),
+      HOMEF: null, // await getLastTenConsumptionFrom(HOMEF),
+    };
+    return { data };
+  },
+  getResidanceLaundryConsumption: async (toDate = lastDate, sort = DESC) => {
+    const fromDate = getLastMonthDate(toDate);
+    const data = {
+      HOMEA: null, // await getLastTenConsumptionFrom(HOMEA),
+      HOMEB: null, // await getLastTenConsumptionFrom(HOMEB),
+      HOMEC: null, // await getLastTenConsumptionFrom(HOMEC),
+      HOMED: null, // await getLastTenConsumptionFrom(HOMED),
+      HOMEE: null, // await getLastTenConsumptionFrom(HOMEE),
+      HOMEF: null, // await getLastTenConsumptionFrom(HOMEF),
+    };
+    return { data };
+  },
+});
+
+function getLastMonthDate(startDate) {
+  const lastMonthDate = new Date(startDate);
+  lastMonthDate.setDate(startDate.getDate() - 30);
+  return lastMonthDate;
+}
+
+export default SensorsService;
