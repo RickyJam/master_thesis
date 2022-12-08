aa2281c6ae94564fb5bd9191668fbb5927c16adb
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-08 17:42:28 +0100

    collegato sensros api

--- components/residence_server/src/api/sensors_api.js
+++ components/residence_server/src/api/sensors_api.js
@@ -1,6 +1,6 @@
-// import SensorsService from "../services/sensors_service.js";
+import SensorsService from "../services/sensors_service.js";
 
-// const sensorsService = SensorsService();
+const sensorsService = SensorsService();
 
 const SENSORS_PATH = "/residence/:home";
 const KITCHENS_PATH = SENSORS_PATH + "/kitchens";
@@ -12,19 +12,19 @@ const SensorsApi = (server) => ({
   register: () => {
     server.get(SENSORS_PATH, async (req, res) => {
       const home = req.params[HOME_KEY];
-      const data = {}; // await sensorsService.getResidanceSensors();
+      const data = await sensorsService.getHomeSensors();
       res.send(data);
     });
 
     server.get(KITCHENS_PATH, async (req, res) => {
       const home = req.params[HOME_KEY];
-      const data = {}; //await sensorsService.getResidanceKitchensSensors();
+      const data = await sensorsService.getHomeKitchenSensors();
       res.send(data);
     });
 
     server.get(LAUNDRY_PATH, async (req, res) => {
       const home = req.params[HOME_KEY];
-      const data = {}; //await sensorsService.getResidanceLaundrySensors();
+      const data = await sensorsService.getHomeLaundrySensors();
       res.send(data);
     });
   },
