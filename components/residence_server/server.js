158f6cbb3cd4b80f2371b239304e63c8056361de
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-08 17:31:41 +0100

    registrate nuove api

--- components/residence_server/server.js
+++ components/residence_server/server.js
@@ -1,14 +1,16 @@
 import ConsumptionApi, { getConsumptionPaths } from "./src/api/consumption_api.js";
 import express from "express";
 import isDev from "./src/utils/enviroment.js";
-import { getSensorsPaths } from "./src/api/sensors_api.js";
+import SensorsApi, { getSensorsPaths } from "./src/api/sensors_api.js";
 
 const PORT = 3000;
 
 const server = express();
 const consumption_api = ConsumptionApi(server);
+const sensors_api = SensorsApi(server);
 
 consumption_api.register();
+sensors_api.register();
 
 server.listen(PORT, () =>
   console.log("Node server started and listening on port 3000")
