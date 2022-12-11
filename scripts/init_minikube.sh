4f95c79af96ace1fa3f933ab804d61b1030c0559
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-11 22:23:30 +0100

    modificato script

--- scripts/init_minikube.sh
+++ scripts/init_minikube.sh
@@ -15,5 +15,8 @@ kubectl config set-context minikube --namespace=master-thesis
 # creazione folder dataset
 ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/dataset && sudo chmod 777 /mnt/dataset"
 
-# creazione mount data
+# creazione mount data db-dataset
+ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/data"
+
+# creazione mount data db-users
 ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/data"
