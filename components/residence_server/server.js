ec88b496408c0ea1148d688670b5838998e0cec2
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-17 00:35:37 +0100

    utilizzo di middleware per auth di tutte le rotte

--- components/residence_server/server.js
+++ components/residence_server/server.js
@@ -2,14 +2,18 @@ import ConsumptionApi, { getConsumptionPaths } from "./src/api/consumption_api.j
 import express from "express";
 import isDev from "./src/utils/enviroment.js";
 import SensorsApi, { getSensorsPaths } from "./src/api/sensors_api.js";
+import AuthMiddleware from "./src/middleware/auth_middleware.js";
 
 const PORT = 3000;
 
 const server = express();
-const consumption_api = ConsumptionApi(server);
-const sensors_api = SensorsApi(server);
+const auth_middleware = AuthMiddleware(server);
+auth_middleware.register();
 
+const consumption_api = ConsumptionApi(server);
 consumption_api.register();
+
+const sensors_api = SensorsApi(server);
 sensors_api.register();
 
 server.listen(PORT, () =>
