be3d90a8d4a50b9cd1a9f8b2721a4b8de51835c4
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-16 19:40:46 +0100

    creata find per lettura user da db

--- components/residence_server/src/dao/user_authorization.js
+++ components/residence_server/src/dao/user_authorization.js
@@ -4,7 +4,9 @@ const AUTHORIZATIONS_COLLECTION = "authorizations";
 const USERS_COLLECTION = "users";
 
 async function getUser(userId) {
-
+  return await onUserDB((db) =>
+    db.collection(USERS_COLLECTION).findOne({ userId: userId })
+  );
 }
 
 async function getAuthorizationsFor(user) {
