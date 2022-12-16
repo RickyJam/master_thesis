f26f49fc24d8f4e0f8fd56a09730488a59f96c1f
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-16 19:40:11 +0100

    creato user auth per accesso a db

---
+++ components/residence_server/src/dao/user_authorization.js
@@ -0,0 +1,14 @@
+import onUserDB from "./user_access.js";
+
+const AUTHORIZATIONS_COLLECTION = "authorizations";
+const USERS_COLLECTION = "users";
+
+async function getUser(userId) {
+
+}
+
+async function getAuthorizationsFor(user) {
+
+}
+
+export { getAuthorizationsFor, getUser };
