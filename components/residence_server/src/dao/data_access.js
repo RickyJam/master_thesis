463f608803aaf3f00b38bd8a66db0e1438d93a93
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-16 16:28:32 +0100

    refactor estrazione parti in comune

--- components/residence_server/src/dao/data_access.js
+++ components/residence_server/src/dao/data_access.js
@@ -1,33 +1,9 @@
-import { MongoClient } from "mongodb";
-import isDev from "../utils/enviroment.js";
+import onMasterDB from "./mongo_access.js";
 
-const DB_NAME = "master";
-const k8s_host = "mongodb-service:27017";
-const dev_host = "localhost:27017";
+const k8s_data_host = "mongodb-service:27017";
 
-function getDbUri() {
-  const hostname = isDev() ? dev_host : k8s_host;
-  return `mongodb://admin:password@${hostname}/?authMechanism=DEFAULT&authSource=${DB_NAME}`;
+async function onDataDB(query) {
+  return await onMasterDB(query, k8s_data_host);
 }
 
-function getMongoClient() {
-  const uri = getDbUri();
-  return new MongoClient(uri);
-}
-
-async function onMasterDB(query) {
-  const client = getMongoClient();
-  try {
-    await client.connect();
-    const db = client.db(DB_NAME);
-
-    return await query(db);
-  } catch (e) {
-    console.log("Error: " + e);
-    return undefined;
-  } finally {
-    await client.close();
-  }
-}
-
-export default onMasterDB;
+export default onDataDB;
