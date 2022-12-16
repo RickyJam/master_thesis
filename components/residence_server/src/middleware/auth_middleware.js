027a2c362c53787bbbcb8fb2dc23561ca1350845
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-17 00:35:18 +0100

    creato middleware

---
+++ components/residence_server/src/middleware/auth_middleware.js
@@ -0,0 +1,29 @@
+import UsersService from "../services/users_service.js";
+
+const USERID_KEY = "userId";
+const usersService = UsersService();
+
+const AuthMiddleware = (server) => ({
+  register: () => {
+    const auth = async (req, res, next) => {
+      const userId = req.query[USERID_KEY];
+
+      const user = await usersService.authenticate(userId);
+      if (invalidUser(user)) {
+        res.status(401).send("User not found!");
+        return;
+      }
+
+      req.user = user;
+      next();
+    };
+
+    server.use(auth);
+  },
+});
+
+function invalidUser(user) {
+  return user === undefined;
+}
+
+export default AuthMiddleware;
