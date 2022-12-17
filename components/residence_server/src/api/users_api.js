d14c31af0b7c939d50f3e982f4db155d2baa8ab1
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-17 18:15:15 +0100

    creato users Api

---
+++ components/residence_server/src/api/users_api.js
@@ -0,0 +1,34 @@
+import UsersService from "../services/users_service.js";
+
+const usersService = UsersService();
+
+const USERS_PATH = "/residence/users";
+const LOGIN_PATH = USERS_PATH + "/login";
+const AUTHORIZATIONS_PATH = USERS_PATH + "/authorizations";
+
+//test routes
+const UsersApi = (server) => ({
+  register: () => {
+    server.get(USERS_PATH, async (req, res) => {
+        const authorizations = await usersService.getAllUsers();
+  
+        res.send({ data: authorizations });
+      });
+
+    server.get(LOGIN_PATH, async (req, res) => {
+      const authorizations = await usersService.getAuthorizations(req.user);
+
+      res.send({ data: authorizations });
+    });
+
+    server.get(AUTHORIZATIONS_PATH, async (req, res) => {
+      const authorizations = await usersService.getAllAuthorizations();
+
+      res.send({ data: authorizations });
+    });
+  },
+});
+
+
+export const getUsersPaths = [USERS_PATH, LOGIN_PATH, LAUNDRY_PATH];
+export default UsersApi;
