ccf6d283fbb60292e1ff668e6daafc233286ec43
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-17 17:15:59 +0100

    aggiunto log

--- components/users_loader/users_loader.js
+++ components/users_loader/users_loader.js
@@ -4,6 +4,7 @@ import authorizations from "./src/authorizations.js";
 
 async function loadAuths() {
   for (const auths of authorizations) {
+    console.log("inserting: " + auths[0].relatedRode);
     await insertAuthorizations(auths);
   }
 }
