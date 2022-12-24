b26156a278e5e51cede5237253ace2ec6c1b7de3
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-24 22:37:49 +0100

    create date

--- components/residence_server/src/utils/date_helper.js
+++ components/residence_server/src/utils/date_helper.js
@@ -5,3 +5,25 @@ export function getLastMonthDate(startDate) {
   lastMonthDate.setDate(startDate.getDate() - 30);
   return lastMonthDate;
 }
+
+function getFollowingThirtyDays(fromDate) {
+  const lastMonthDate = new Date(fromDate);
+  lastMonthDate.setDate(startDate.getDate() + 30);
+  return lastMonthDate;
+}
+
+function getPastThirtyDays(toDate) {
+  const lastMonthDate = new Date(toDate);
+  lastMonthDate.setDate(startDate.getDate() - 30);
+  return lastMonthDate;
+}
+
+export function getDates({ from, to }) {
+  if (from) {
+    return { fromDate: from, toDate: getFollowingThirtyDays(from) };
+  } else if (to) {
+    return { fromDate: getPastThirtyDays(toDate), toDate: to };
+  } else {
+    return { fromDate: getPastThirtyDays(lastDate), toDate: lastDate };
+  }
+}
