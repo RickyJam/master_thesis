ab2d6476dce91602412c96fac2fad6b8d1fc4e59
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-28 00:09:43 +0100

    creati n processi in base a ogni utente

--- components/users_simulator/users_simulator.py
+++ components/users_simulator/users_simulator.py
@@ -3,5 +3,15 @@ from src import users
 
 users_list = users.users
 
+def executor(user):
+    print(user)
+
 if __name__ == "__main__":
-    print(users_list)
+    users_process = []
+
+    for user in users_list:
+        t = threading.Thread(target=executor, args=(user,))
+        users_process.append(t)
+        t.start()
+    
+    # TODO: quanto andare a avanti?
