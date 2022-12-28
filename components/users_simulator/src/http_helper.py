f4bcc6e57ba67acbc4b5fffd71a49bb03323cc8e
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-28 19:54:44 +0100

    effettuate chiamate randomiche al server

--- components/users_simulator/src/http_helper.py
+++ components/users_simulator/src/http_helper.py
@@ -1,15 +1,49 @@
 import requests
+import random
 
-urls = [
+IS_K8S = False
 
+LOCAL_HOST = 'localhost:3000'
+K8S_HOST = 'resad'  # TODO: completare
+
+urls: list = [
+    # consumption
+    lambda _: '/residence/power',
+    lambda _: '/residence',
+    lambda _: '/residence/kitchens',
+    lambda _: '/residence/laundry',
+    lambda _: '/residence/power',
+
+    # sensors
+    lambda home: f'/residence/{home}',
+    lambda home: f'/residence/{home}/kitchens',
+    lambda home: f'/residence/{home}/laundry',
 ]
 
 
-def doRequest(user: dict):
-    params = {'userId': user["userId"]}
+def __getHostUrl() -> str:
+    if IS_K8S:
+        return f'http://{K8S_HOST}'
+    else:
+        return f'http://{LOCAL_HOST}'
+
+
+def __buildRandomUrl(user):
+    url = urls[random.randint(0, len(urls))](user["home"][0])
+    return f'{__getHostUrl()}{url}'
+
+
+def __buildParams(user: dict) -> dict:
+    return {'userId': user['userId']}
+
+
+def __doRandomRequest(user: dict) -> None:
     try:
-        response = requests.get(
-            'http://localhost:3000/residence/power', params=params)
-        print(response)
+        url = __buildRandomUrl(user)
+        requests.get(url, params=__buildParams(user))
     except:
-        print("request failed")
+        print('request failed')
+
+
+def doRequest(user: dict):
+    __doRandomRequest(user)
