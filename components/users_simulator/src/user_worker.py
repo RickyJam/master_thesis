26189dc783ae0e1f84a98ef15982c7a94f5c2f56
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-28 19:31:23 +0100

    refactor e creata prima chiamata di rete

--- components/users_simulator/src/user_worker.py
+++ components/users_simulator/src/user_worker.py
@@ -9,7 +9,7 @@ MILLIS_PER_SECOND: int = 1000
 N_REQS_PER_MINUTE: int = 10
 
 
-def printCurrentTime() -> None:
+def __printCurrentTime() -> None:
     current_time = datetime.now().strftime("%H:%M:%S")
     print(f'Current Time ={current_time}')
 
@@ -18,23 +18,54 @@ def __getSecondsToWait() -> float:
     return SECONDS_PER_MINUTE / N_REQS_PER_MINUTE
 
 
-def randomWait(event: threading.Event):
+def __randomWait(event: threading.Event):
     event.wait(random.randint(0, 9))
 
+def __doRequest(user: dict):
+    params = {'userId': user["userId"]}
+    try:
+        response = requests.get('http://localhost:3000/residence/power', params=params)
+        print(response)
+    except:
+        print("request failed")
+
+# {   http://localhost:3000/residence/power?userId=residenceOwner
+#   "enviroment": "DEV",
+#   "path": {
+#     "consumption": [
+#       "/residence",
+#       "/residence/kitchens",
+#       "/residence/laundry",
+#       "/residence/power"
+#     ],
+#     "sensors": [
+#       "/residence/:home",
+#       "/residence/:home/kitchens",
+#       "/residence/:home/laundry"
+#     ],
+#     "tests": [
+#       "/users",
+#       "/users/login",
+#       "/users/authorizations"
+#     ]
+#   }
+# }
 
 def run(user: dict, event: threading.Event) -> None:
-    randomWait(event)
+    __randomWait(event)
 
     millisToWait = __getSecondsToWait()
 
-    printCurrentTime()
+    __printCurrentTime()
 
     for i in range(0, N_REQS_PER_MINUTE):
         print(f'Esecuzione request {i} from: {user["userId"]}')
-        printCurrentTime()
+
+        __doRequest(user)
+
         delta = random.randint(1, 4)
         event.wait(millisToWait+delta)
 
-    printCurrentTime()
+    __printCurrentTime()
 
     return
