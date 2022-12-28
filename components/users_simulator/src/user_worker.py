2650b55e4923bb5d42dab170ed03289f66b6765f
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-28 19:18:32 +0100

    aggiunta un po di asincronicità tra utenti

--- components/users_simulator/src/user_worker.py
+++ components/users_simulator/src/user_worker.py
@@ -1,8 +1,41 @@
 import requests
 import threading
+from datetime import datetime
+import random
 
-def run(user: dict, event: threading.Event):
-    print(user)
-    event.wait(1)
-    
-    # TODO: definire un exit point da qui
+SECONDS_PER_MINUTE: int = 60
+MILLIS_PER_SECOND: int = 1000
+
+N_REQS_PER_MINUTE: int = 10
+
+
+def printCurrentTime():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+
+def __getSecondsToWait() -> float:
+    return SECONDS_PER_MINUTE / N_REQS_PER_MINUTE
+
+
+def randomWait(event: threading.Event):
+    event.wait(random.randint(0, 9))
+
+
+def run(user: dict, event: threading.Event) -> None:
+    randomWait(event)
+
+    millisToWait = __getSecondsToWait()
+
+    printCurrentTime()
+
+    for _ in range(0, N_REQS_PER_MINUTE):
+        print("esecuzione request")
+        printCurrentTime()
+        delta = random.randint(1, 4)
+        event.wait(millisToWait+delta)
+
+    printCurrentTime()
+
+    return
