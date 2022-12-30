43e6a69f72e11390ddfebc94efb06ed0edc72cc6
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-30 16:28:32 +0100

    corretto init minikube

--- scripts/init_minikube.sh
+++ scripts/init_minikube.sh
@@ -12,11 +12,11 @@ kubectl label nodes minikube size=large
 # using correct context
 kubectl config set-context minikube --namespace=master-thesis
 
-# creazione folder dataset
-ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/dataset && sudo chmod 777 /mnt/dataset"
-
-# creazione mount data db-dataset
-ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/users"
+# creazione folder dataset -- USARE JOB
+# ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/dataset && sudo chmod 777 /mnt/dataset"
 
 # creazione mount data db-users
+ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/users"
+
+# creazione mount data db-data
 ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/data"
