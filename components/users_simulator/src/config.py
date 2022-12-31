78f15d47b5c1eabecba5a9e46cfa10bd71db4dba
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-31 13:26:34 +0100

    correto file

--- components/users_simulator/src/config.py
+++ components/users_simulator/src/config.py
@@ -1,6 +1,6 @@
 IS_K8S = False
 
 LOCAL_HOST = 'localhost:3000'
-K8S_HOST = 'completare'  # va scritto di volta in volta, cambia ad ogni avvio del cluster
+K8S_HOST = '127.0.0.1:XXXX'  # va scritto di volta in volta, cambia ad ogni avvio del cluster
 
 N_REQS_PER_MINUTE: int = 10
