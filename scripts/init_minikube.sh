88ae92bfdcc74e2a9b71887beeec4a13e07b0e52
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-12-31 12:38:47 +0100

    orretto init_minikube

--- scripts/init_minikube.sh
+++ scripts/init_minikube.sh
@@ -12,8 +12,9 @@ kubectl label nodes minikube size=large
 # using correct context
 kubectl config set-context minikube --namespace=master-thesis
 
-# creazione folder dataset -- USARE JOB
-# ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/dataset && sudo chmod 777 /mnt/dataset"
+# creazione folder dataset_csv
+ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/csv_dataset && sudo chmod 777 /mnt/csv_dataset"
+scp -i ~/.minikube/machines/minikube/id_rsa -P $PORT -r Datasets/csv/* docker@127.0.0.1:/mnt/csv_dataset
 
 # creazione mount data db-users
 ssh -t -i ~/.minikube/machines/minikube/id_rsa -p $PORT docker@127.0.0.1 "sudo mkdir /mnt/users"
