5f5ef80ea5360da47e7b993bc9ca9d708dcc568f
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2023-01-13 18:12:33 +0100

    aggiunto commento

--- components/users_simulator/src/config.py
+++ components/users_simulator/src/config.py
@@ -1,6 +1,10 @@
-IS_K8S = False
+IS_K8S = True
 
 LOCAL_HOST = 'localhost:3000'
-K8S_HOST = '127.0.0.1:XXXX'  # va scritto di volta in volta, cambia ad ogni avvio del cluster
 
-N_REQS_PER_MINUTE: int = 10
+# Porta va scritta ad ogni avvio del cluster, può cambiare: minikube service --url node-server-service -n master-thesis
+K8S_HOST = '127.0.0.1:XXXXX'
+
+
+N_REQS_PER_MINUTE: int = 1000
+
