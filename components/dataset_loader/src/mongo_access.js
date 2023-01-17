2e69b6fa74914cc2c748f243038acccb01474798
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2023-01-17 19:05:42 +0100

    corretot nome

--- components/dataset_loader/src/mongo_access.js
+++ components/dataset_loader/src/mongo_access.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import isDev from "./enviroment.js";
 
 export const DB_NAME = "master";
-const k8s_host = "mongodb-service:27017";
+const k8s_host = "datadb-service:27017";
 const dev_host = "localhost:27017";
 
 function getDbUri() {
