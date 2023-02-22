53bda8a7ae5a97a73a666b87dcad25a72da21085
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2023-02-22 20:04:32 +0100

    corretto funzionamento e ritornata la lista di tempi impiegati

--- components/users_simulator/src/user_worker.py
+++ components/users_simulator/src/user_worker.py
@@ -1,9 +1,10 @@
-from threading import Event
+from threading import Event, Thread
 import random
 from src.http_helper import doAsyncRequest
 from src.config import N_REQS_PER_MINUTE, MINUTES_EXECUTION
 
 SECONDS_PER_MINUTE: int = 60
+
 
 def __getSecondsToWait() -> float:
     return SECONDS_PER_MINUTE / N_REQS_PER_MINUTE
@@ -13,16 +14,24 @@ def __randomWait(event: Event):
     event.wait(random.randint(0, 9))
 
 
-def run(user: dict, event: Event) -> None:
-    __randomWait(event)
+def run(user: dict, event: Event, users_results: list[list[int]], userIndex: int) -> None:
+    totalRequests = N_REQS_PER_MINUTE * MINUTES_EXECUTION
+    requestThreads: list[Thread] = []
+    resultsList: list[int] = [None] * totalRequests
+
+    # __randomWait(event)
 
     millisToWait = __getSecondsToWait()
 
-    for i in range(0, N_REQS_PER_MINUTE * MINUTES_EXECUTION):
+    for i in range(0, totalRequests):
         print(f'Esecuzione request {i} from: {user["userId"]}')
 
-        doAsyncRequest(user)
+        requestThreads.append(doAsyncRequest(user, resultsList, i))
 
         event.wait(millisToWait)
 
-    return
+    for thread in requestThreads:
+        thread.join()
+
+    users_results[userIndex] = resultsList
+    return 
