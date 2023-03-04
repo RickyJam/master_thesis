5d1da4af1ff8983285a2b9aabbab562b90121125
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2023-03-04 15:56:11 +0100

    aggiunto contegigo totale del empo impiegato per smaltire i lavori

--- components/users_simulator/users_simulator.py
+++ components/users_simulator/users_simulator.py
@@ -1,9 +1,11 @@
 from threading import Event, Thread
 from src import users, user_worker
+import time
 
 users_list = users.users
 
 def main():
+    start = time.perf_counter()
     event: Event = Event()
     user_threads: list = []
     users_results: list = [None] * len(users_list)
@@ -25,6 +27,8 @@ def main():
         print(users_results[i])
         failed_request += users_results[i].count(60)
     print(f'Total failed requests: {failed_request}')
+    print(f'Total ellapsed time in seconds: {time.perf_counter() - start}')
+    print(f'Total ellapsed time in minutes: {(time.perf_counter() - start) / 60}')
 
 
 if __name__ == "__main__":
